3653d43566cc7c347e55e50985f0ab7a8727cc78
JohnxdLink <JohnxdLink@gmail.com>
2024-01-16 10:18:49 +0800

    Msg: 1st Commit Adding Developer Comment

--- Index.aspx.cs
+++ Index.aspx.cs
@@ -7,8 +7,16 @@ using System.Web.UI.WebControls;
 
 namespace CTU_Danao_Discussion_Room_Reservation_System
 {
+    /* 
+    * Project       : CTU-Danao Discussion Room Rservation System
+    * Developer     : Castro JOhn Christian
+    * Message       : This is under development
+    * Date Started  : 01/15/2024
+    */
+
     public partial class Index : System.Web.UI.Page
     {
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
